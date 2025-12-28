aef9cf4f4a3fa9832a14949905cb8b9fc66c8d78
Denys Sichka <sickadenis@gmail.com>
2025-12-28 16:19:05 +0100

    cors fix

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const tasksRoutes = require('./routes/tasksRoutes')
 const app = express()
 
 const corsOptions = {
-  origin: ['http://localhost:3000, https://kanban-board-228.vercel.app/'],
+  origin: [
+    'http://localhost:3000, https://kanban-board-228-er6tfv9kx-alienqqes-projects.vercel.app/',
+  ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true,
 }
